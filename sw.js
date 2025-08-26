a52193fb662060316259391ee71a671e098c5bdc
Kushal Dahal <110470070+KushalDahal00@users.noreply.github.com>
2025-08-26 12:10:46 +0545

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
+self.addEventListener('install', event => { event.waitUntil(self.skipWaiting()); });
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => { /* basic fetch handling */ });
